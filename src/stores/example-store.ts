ec4fdb35ff17df189d8a6d8c0bda164fd00be28c
Villeman <villeman.arm@gmail.com>
2024-06-25 10:00:49 +0300

    initial commit

---
+++ src/stores/example-store.ts
@@ -0,0 +1,10 @@
+import { ref, computed, watch } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', () => {
+    const counter = ref(0)
+    const doubleCount = computed(() => counter.value * 2)
+    const increment = () => counter.value++
+
+    return { counter, doubleCount, increment }
+})
