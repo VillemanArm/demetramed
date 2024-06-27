2037a16fa6e56a2162e93efc7bdc7f4e8f195a19
Villeman <villeman.arm@gmail.com>
2024-06-27 08:10:34 +0300

    feat: plug openseadragon

---
+++ src/components/ImageViewer.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {reactive, ref, computed, onMounted, onUpdated, watch} from 'vue'
+import OpenSeadragon from 'openseadragon'
+
+// defineProps<{
+// 	msg: string;
+// }>();
+
+onMounted(() => {
+    const viewer = OpenSeadragon({
+        id: 'openseadragon1',
+        prefixUrl: '/img/openseadragon_images/', // перед деплоем поменять путь на /img/openseadragon_images/
+        tileSources: {
+            type: 'image',
+            url: '/img/test_img.jpg',
+            // buildPyramid: false,
+        },
+    })
+    //https://insight-webstudio.ru/files_for_another_projects/slide-2022-09-12T15-38-25-R1-S2.mrxs
+})
+</script>
+
+<template>
+    <div class="">
+        <h1>OpenSeadragon</h1>
+        <div
+            id="openseadragon1"
+            style="width: 800px; height: 600px"
+        ></div>
+    </div>
+</template>
+
+<style scoped lang="sass"></style>
