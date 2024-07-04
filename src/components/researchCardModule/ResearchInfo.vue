b617cac0d0bb7a3d3911558fe90533efad02a281
Villeman <villeman.arm@gmail.com>
2024-07-04 16:37:15 +0300

    feat: add components structure to research card module

---
+++ src/components/researchCardModule/ResearchInfo.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="info">
+        <span>Дата:</span>
+        <span>01/07/2023</span>
+        <span>№Исследования:</span>
+        <span>0250330</span>
+        <span>Пациент:</span>
+        <span>Панкратова Е.Н.</span>
+        <span>Возраст:</span>
+        <span>35 лет</span>
+        <span>Учреждение по направлению:</span>
+        <span>ГБУЗ НО Поликлиника №30</span>
+        <span>ФИО врача по направлению:</span>
+        <span>Черногорцева Е.С.</span>
+        <span>Клинический диагноз:</span>
+        <span>-</span>
+        <span>Комментарий:</span>
+        <span>-</span>
+
+        <EditCircleIcon class="info__edit" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import {reactive, ref, computed, onMounted, onUpdated, watch} from 'vue'
+import EditCircleIcon from 'assets/icons/edit-circle-icon.vue'
+
+//defineProps<{
+//	msg: string;
+//}>();
+</script>
+
+<style scoped lang="sass">
+.info
+    width: 513rem
+    padding: 22rem 32rem 72rem 32rem
+    margin-bottom: 32rem
+    display: grid
+    grid-template-columns: 1fr 1fr
+    gap: 18rem 44rem
+    position: relative
+
+    border: 1rem solid $non-active-color
+    border-radius: 4rem
+
+.info__edit
+      width: 60rem
+      height: 60rem
+      position: absolute
+      bottom: 24rem
+      right: 24rem
+
+      cursor: pointer
+
+      &:hover
+        fill: $active-color
+</style>
--- src/modules/ResearchCardModule.vue
+++ src/modules/ResearchCardModule.vue
@@ -7,96 +7,20 @@
         >
 
         <div class="card__wrap">
-            <div>
-                <div class="card__info">
-                    <span>Дата:</span>
-                    <span>01/07/2023</span>
-                    <span>№Исследования:</span>
-                    <span>0250330</span>
-                    <span>Пациент:</span>
-                    <span>Панкратова Е.Н.</span>
-                    <span>Возраст:</span>
-                    <span>35 лет</span>
-                    <span>Учреждение по направлению:</span>
-                    <span>ГБУЗ НО Поликлиника №30</span>
-                    <span>ФИО врача по направлению:</span>
-                    <span>Черногорцева Е.С.</span>
-                    <span>Клинический диагноз:</span>
-                    <span>-</span>
-                    <span>Комментарий:</span>
-                    <span>-</span>
-                    <div class="card__info-edit">
-                        <EditCircleIcon />
-                    </div>
-                </div>
-
+            <div class="card__info">
+                <ResearchInfo />
                 <BaseButton
-                    v-if="false"
                     class="card__button"
                     label="Начать анализ"
                 >
                     <AnalysisIcon />
                 </BaseButton>
 
-                <AnalysisReport />
+                <AnalysisReport v-if="false" />
             </div>
             <div class="card__viewer">
                 <ImageViewer class="card__image-viewer" />
-                <div class="card__atypical-objects">
-                    <h3 class="card__atypical-objects-title">
-                        Атипичные объекты
-                    </h3>
-                    <div class="card__atypical-objects-list">
-                        <div class="card__atypical-objects-item">
-                            <h4 class="card__atypical-objects-item-title">
-                                Объект #AO1
-                            </h4>
-                            <div class="card__atypical-objects-item-img">
-                                <img src="/img/snapshot-item-example.jpg" />
-                            </div>
-                        </div>
-                        <div class="card__atypical-objects-item">
-                            <h4 class="card__atypical-objects-item-title">
-                                Объект #AO1
-                            </h4>
-                            <div class="card__atypical-objects-item-img">
-                                <img src="/img/snapshot-item-example.jpg" />
-                            </div>
-                        </div>
-                        <div class="card__atypical-objects-item">
-                            <h4 class="card__atypical-objects-item-title">
-                                Объект #AO1
-                            </h4>
-                            <div class="card__atypical-objects-item-img">
-                                <img src="/img/snapshot-item-example.jpg" />
-                            </div>
-                        </div>
-                        <div class="card__atypical-objects-item">
-                            <h4 class="card__atypical-objects-item-title">
-                                Объект #AO1
-                            </h4>
-                            <div class="card__atypical-objects-item-img">
-                                <img src="/img/snapshot-item-example.jpg" />
-                            </div>
-                        </div>
-                        <div class="card__atypical-objects-item">
-                            <h4 class="card__atypical-objects-item-title">
-                                Объект #AO1
-                            </h4>
-                            <div class="card__atypical-objects-item-img">
-                                <img src="/img/snapshot-item-example.jpg" />
-                            </div>
-                        </div>
-                        <div class="card__atypical-objects-item">
-                            <h4 class="card__atypical-objects-item-title">
-                                Объект #AO1
-                            </h4>
-                            <div class="card__atypical-objects-item-img">
-                                <img src="/img/snapshot-item-example.jpg" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <AtypicalObjectsList />
             </div>
         </div>
     </div>
@@ -105,10 +29,11 @@
 <script setup lang="ts">
 import {reactive, ref, computed, onMounted, onUpdated, watch} from 'vue'
 import ImageViewer from 'src/ui/ImageViewer.vue'
-import EditCircleIcon from 'assets/icons/edit-circle-icon.vue'
+
 import BaseButton from 'src/ui/BaseButton.vue'
 import AnalysisIcon from 'assets/icons/analysis-icon.vue'
-
+import ResearchInfo from 'components/researchCardModule/ResearchInfo.vue'
+import AtypicalObjectsList from 'components/researchCardModule/AtypicalObjectsList.vue'
 import AnalysisReport from 'components/researchCardModule/AnalysisReport.vue'
 
 //defineProps<{
@@ -119,109 +44,42 @@ import AnalysisReport from 'components/researchCardModule/AnalysisReport.vue'
 </script>
 
 <style scoped lang="sass">
-svg
-    width: 100%
-    height: 100%
-    object-fit: contain
-
 .card
   width: 100%
-  padding: 8rem 84rem 0 84rem
+  padding: 8rem 84rem 52rem 84rem
 
-  &__back
-    display: block
-    margin-bottom: 16rem
+.card__back
+  display: block
+  margin-bottom: 16rem
 
-    color: $non-active-color
+  color: $non-active-color
 
-    &:hover
-      color: $active-color
+  &:hover
+    color: $active-color
 
-  &__wrap
-    width: 100%
-    display: flex
+.card__wrap
+  width: 100%
+  display: flex
 
-    &>div:first-child
-      margin-right: 44rem
+.card__info
+  margin-right: 44rem
 
-  &__info
-    width: 513rem
-    padding: 22rem 32rem 72rem 32rem
-    margin-bottom: 32rem
-    display: grid
-    grid-template-columns: 1fr 1fr
-    gap: 18rem 44rem
-    position: relative
+.card__button
+  padding: 18rem 14rem
+  margin-bottom: 32rem
+  display: flex
+  align-items: center
 
-    border: 1rem solid $non-active-color
-    border-radius: 4rem
+  font-size: 18rem
 
-    &-edit
-      width: 60rem
-      height: 60rem
-      position: absolute
-      bottom: 24rem
-      right: 24rem
+  &>svg
+    width: 24rem
+    height: 24rem
 
-      cursor: pointer
-
-      & svg:hover
-        fill: $active-color
-
-  &__button
-    padding: 18rem 14rem
-    margin-bottom: 32rem
-    display: flex
-    align-items: center
-
-    font-size: 18rem
-
-    &>svg
-      width: 24rem
-      height: 24rem
-
-      transition: all 0.3s ease-in-out
-
-    &:hover svg
-      fill: $active-color
+  &:hover svg
+    fill: $active-color
 
 .card__viewer
   display: flex
-
-.card__image-viewer
-  margin-right:55rem
-
-.card__atypical-objects-title
-  margin-bottom: 32rem
-
-  font-size: 20rem
-  line-height: 120%
-  font-weight: 600
-  white-space: nowrap
-
-  color: $active-color
-
-.card__atypical-objects-item-title
-  margin-bottom: 6rem
-
-  font-size: 18rem
-  line-height: 120%
-
-.card__atypical-objects-item-img
-  width: 224rem
-  height: 156rem
-
-  cursor: pointer
-
-.card__atypical-objects-list
-  height: 822rem
-  display: grid
-  gap: 18rem
-
-  overflow-y: auto
-  overflow-x: hidden
-  scrollbar-width: none
-
-  &::-webkit-scrollbar
-    display: none
+  gap: 55rem
 </style>
